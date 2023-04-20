5cd2c0d2aa1c2f27942639fd8b6c331126d680ae
UKeeShon <ysheng04@email.unc.edu>
2023-04-20 15:59:59 -0400

    10

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,7 +5,7 @@ import minimist from "minimist";
 let args = minimist(process.argv.slice(2));
 
 if (args.h || args.help){
-	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\t-h, --help     display this help message and exit\n\t-r, --rules     display the rules and exit\n\nExaamples:\n\tnode-rps     Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result","result","win"}');
+	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\n\t-h, --help     display this help message and exit\n\t-r, --rules     display the rules and exit\n\nExaamples:\n\tnode-rps     Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result","win"}');
 	process.exit(0);
 }
 
@@ -18,7 +18,7 @@ let shot = args._[0];
 try{
 	console.log(JSON.stringify(rps(shot)));
 } catch (e){
-	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\N\T-h, --help     display this help message and exit\n\t\-r, --rules     display the rules and exit\n\nExamples:\n\tnode-rps     Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result","result":"win"}');
+	console.log('Usage: node-rps [SHOT]\nPlay Rock Paper Scissors (RPS)\N\T-h, --help     display this help message and exit\n\t\-r, --rules     display the rules and exit\n\nExamples:\n\tnode-rps     Return JSON with single player RPS result.\n\t\t\t\te.g. {"player":"rock"}\n\tnode-rps rock   Return JSON with results for RPS played against a simulated opponent.\n\t\t\t\te.g {"player":"rock","opponent":"scissors","result":"win"}');
 	console.log('Rules for Rock Paper Scissors:\n\n\t- Scissors CUTS Paper\n\t- Paper COVERS Rock\n\t- Rock CRUSHES Scissors');
 	process.exit(0);
 }
